236623e3e11c8a7ceb4900d88ee6a7d65086b0bf
Tyler <Walkert20@hanover.edu>
2021-12-11 01:05:41 -0500

    AI for the enemy team.
    I have a framework for how the AI will make it's decisions as well as a parser for decisions and some of the roles for the enemies. Coming along rather smoothly (I think).

--- RGPgame.py
+++ RGPgame.py
@@ -4,32 +4,81 @@ import time
 print ("Hello")
 
 class Player():
-    def __init__(self, name):
-    	self.name = name
-        self.health = 100
-        self.MAX_health = 100
-        self.Atk = 100
-        self.Def = 90
-        self.exp = 0
+    def __init__(name):
+    	name = name
+        health = 100
+        MAX_health = 100
+        Atk = 100
+        Def = 90
+        exp = 0
+        MAx_exp = 100
+        role = None
 
-    def calculate_damage(self, damage, attacker):
-    	if (damage > self.health):
-    		self.health = 0
+    def calculate_damage(Player, damage, attacker):
+    	if (damage > Player.health):
+    		Player.health = 0
     		print("{0} has taken {1} damage from {2}. {0} has died!"
-    			.format(self.name.capitalize(), damage, attacker))
+    			.format(Player.name.capitalize(), damage, attacker))
     	else:
-    		self.health -= damage_amount
+    		Player.health -= damage_amount
             print("{0} takes {1} damage from {2}!"
-            	.format(self.name.capitalize(), damage_amount, attacker))
+            	.format(Player.name.capitalize(), damage_amount, attacker))
 
-    def calculate_heal(self, target_player, heal_amount):
+    def calculate_heal(Player, target_player, heal_amount):
     	if (heal_amount + target_player.health >= target_player.MAX_health):
     		target_player.health = target_player.MAX_health
-    		print("{0} heals {1} back to ull health!"
-    			.format(self.name.capitalize, target_player.name.capitalize))
+    		print("{0} heals {1} back to full health!"
+    			.format(Player.name.capitalize, target_player.name.capitalize))
     	else:
     		target_player += heal_amount
     		print("{0} heals {1} for {2}!"
-    			.format(self.name.capitalize, target_player.name.capitalize, heal_amount))
+    			.format(Player.name.capitalize, target_player.name.capitalize, heal_amount))
 
+def parse_int(input):
+    try:
+        int(input)
+        return True
+    except ValueError:
+        return False
 
+def get_selection():
+    valid_input = False
+    while (valid_input is False):
+        print()
+        choice = input("What will you do?: ")
+        if (parse_int(choice) is True):
+            return int(choice)
+        else:
+            print("The input was invalid. Please try again.")
+
+def enemy_turn(Player):
+	sleep_time = random.randrange(2, 6)
+	print("....enemy turn....")
+	time.sleep(sleep_time)
+
+	if (Player.role == "medic"): #To make things simple, the medic only heals themselves.
+		if (Player.health <=  (.35 x Player.MAX_health)):
+			result = random.randint(1,6)
+			if (result % 2 == 0):
+				return 3
+			else:
+				return random.randint(1,2)
+		else:
+			return random.randint(1,2,3)
+
+	elif (Player.role == "attacker"):
+		if (Player.health <=  (.40 x Player.MAX_health)):
+			result = random.randint(1,6)
+			if (result % 2 == 0):
+				return 2
+			else:
+				return random.randint(1,3)
+		else:
+			return random.randint(1,2,3)
+
+	else:
+		result = random.randint(1,10)
+		if (result <= 4):
+			return 3
+		else:
+			return 1

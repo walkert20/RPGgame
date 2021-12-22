e2af39b602126e7c602176308e416a6603e61fef
Tyler <Walkert20@hanover.edu>
2021-12-21 23:40:17 -0500

    Some work in progress....
    I have more stuff down. It's kind of a skeleton of what I want it to do in the future. LOTS more to work on, but this is what I have for now. Also created test file.

--- RGPgame.py
+++ RGPgame.py
@@ -3,6 +3,10 @@ import time
 
 print ("Hello")
 
+
+items = []
+
+
 class Player():
     def __init__(name):
     	name = name
@@ -11,7 +15,8 @@ class Player():
         Atk = 100
         Def = 90
         exp = 0
-        MAx_exp = 100
+        MAX_exp = 100
+        level = 0
         role = None
 
     def calculate_damage(Player, damage, attacker):
@@ -57,7 +62,7 @@ def enemy_turn(Player):
 	time.sleep(sleep_time)
 
 	if (Player.role == "medic"): #To make things simple, the medic only heals themselves.
-		if (Player.health <=  (.35 x Player.MAX_health)):
+		if (Player.health <=  (.35 * Player.MAX_health)):
 			result = random.randint(1,6)
 			if (result % 2 == 0):
 				return 3
@@ -82,3 +87,60 @@ def enemy_turn(Player):
 			return 3
 		else:
 			return 1
+
+def play_round (player, computer):
+	game_in_progress = True
+	current_player = computer
+
+	while game_in_progress:
+		if (current_player == computer):
+			current_player = player
+		else:
+			current_player = computer
+
+		print()
+		print( "Your move. You have {0} health remaining. "
+			"Your enemy has {1} health remaining. "
+			.format(player.health, computer.health))
+		print()
+
+		if(current_player == player):
+			print("Available moves:")
+			print("1) Basic attack.")
+			print("2) Use item.")
+			print("3) Special move!")
+			choice = get_selection()
+		else:
+			choice = enemy_turn(computer)
+
+		if (choice == 1):
+			damage = random.randrange(player.Atk * .18, player.Atk *.25)
+			if (current_player == player):
+				computer.calculate_damage(computer, damage, player)
+			else:
+				player.calculate_damage(player, damage, computer)
+
+		elif (choice == 2):
+			save()
+			print()
+			print("Your progress has been saved!(Only it hasn't...")
+		elif (choice == 3):
+			heal = random.randrange(player.Def * .20, player.Def * .30)
+			target = player
+			current_player.calculate_heal(player, target, heal)
+		else:
+			print ("That wasn't a valid input. Try again.")
+
+		if (player.health == 0):
+			print("Your team has been wiped out! You lose.")
+		if (computer.health == 0):
+			print("The enemy team has been defeated! Victory!")
+
+
+def start_game():
+	print("The game has started and the fight is on!")
+	keep_playing = True
+
+	while (keep_playing is True):
+		print()
+		play_round()
